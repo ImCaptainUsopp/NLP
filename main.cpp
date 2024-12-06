6ebd44abfdd7b08bd4475c11ce2d73fbd00efb8a
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-12-06 16:02:26 +0200

    Simplify and structure the code

--- main.cpp
+++ main.cpp
@@ -22,12 +22,145 @@ struct hash_pair {
     }
 };
 
-double get_emission_probability(pair<int, unordered_map<string, int>> word_emission, string pos_tag)
+struct Word_emission {
+    long total_count = 0;
+    unordered_map<string, int> post_tag_to_count;
+
+    double probability(string pos_tag) {
+        return 1.0 * post_tag_to_count[pos_tag] / total_count;
+    }
+};
+
+void read_data_files(vector<vector<string>> &sentences, vector<vector<string>> &pos);
+unordered_map<string, Word_emission> build_emission_matrix(vector<vector<string>> sentences, vector<vector<string>> pos_tags);
+unordered_map<pair<string, string>, double, hash_pair> build_transition_probability_matrix(vector<vector<string>> pos_tags);
+
+vector<string> viterbi(unordered_map<string, Word_emission> emission_matrix,
+                       unordered_map<pair<string, string>, double, hash_pair> linear_trans_prob_matrix,
+                       vector<string> sentence);
+
+int main()
 {
-    return 1.0 * word_emission.second[pos_tag] / word_emission.first;
+    vector<vector<string>> sentences, pos_tags;
+
+    read_data_files(sentences, pos_tags);
+
+    auto emission_matrix = build_emission_matrix(sentences, pos_tags);
+
+    auto linear_trans_prob_matrix = build_transition_probability_matrix(pos_tags);
+    
+    /*
+     *  Execution of Viterbi
+     */
+    vector<string> test = {"tom", "loves", "fish"};
+    vector<string> result = viterbi(emission_matrix, linear_trans_prob_matrix, test);
+    cout << "result : ";
+    for (const auto &word : result)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
 
-vector<string> viterbi(unordered_map<string, pair<int, unordered_map<string, int>>> emission_matrix,
+
+
+void read_data_files(vector<vector<string>> &sentences, vector<vector<string>> &pos) {
+    ifstream data("./data.txt");
+    ifstream posdata("./pos.txt");
+
+    if (!data.is_open() || !posdata.is_open()) {
+        cerr << "Error opening file" << endl;
+        return;
+    }
+
+    string line;
+
+    while (getline(data, line)) {
+        if (!line.empty()) {
+            istringstream iss(line);
+            vector<string> words((istream_iterator<string>(iss)), istream_iterator<string>());
+            sentences.push_back(words);
+        }
+    }
+    while (getline(posdata, line)) {
+        if (!line.empty()) {
+            vector<string> words_pos;
+            stringstream ss(line);
+            string word_pos;
+            while (getline(ss, word_pos, ',')) {
+                words_pos.push_back(word_pos);
+            }
+            pos.push_back(words_pos);
+        }
+    }
+    cout << "Number of sentences in input data: " << sentences.size() << endl;
+    cout << "Number of pos tags  in input data: " << pos.size() << endl;
+}
+
+unordered_map<string, Word_emission> build_emission_matrix(vector<vector<string>> sentences, vector<vector<string>> pos_tags) {
+    unordered_map<string, Word_emission> emission_matrix;
+
+    #pragma omp parallel for schedule(dynamic, 10)
+    for (int i = 0; i < sentences.size(); i++) {
+
+        if (sentences[i].size() != pos_tags[i].size()) {
+            cerr << "Words and POS mismatch on line : " << i << endl;
+            continue;
+        }
+
+        for (int j = 0; j < sentences[i].size(); j++)
+        {
+            string word = sentences[i][j];
+            string pos_tag = pos_tags[i][j];
+
+            emission_matrix[word].total_count += 1;
+            emission_matrix[word].post_tag_to_count[pos_tag] += 1;
+        }
+    }
+
+    cout << "Input data has " << emission_matrix.size() << " unique words." << endl;
+    // cout << "fish --> verb : " << emission_matrix["fish"].probability("verb") << ", noun :" << emission_matrix["fish"].probability("noun") << endl;
+
+    return emission_matrix;
+}
+
+unordered_map<pair<string, string>, double, hash_pair> build_transition_probability_matrix(vector<vector<string>> pos_tags) {
+
+    unordered_map<pair<string, string>, double, hash_pair> linear_trans_prob_matrix;
+
+    unordered_map<pair<string, string>, int, hash_pair> transition_to_count;
+    unordered_map<string, int> sum_of_first_word_pos_tag;
+
+    #pragma omp parallel for schedule(dynamic, 10)
+    for (int i = 0; i < pos_tags.size(); i++) {
+        for (int j = 0; j < pos_tags[i].size(); j++) {
+
+            string first_word_pos = j == 0 ? "start" : pos_tags[i][j - 1];
+            string second_word_pos = j == pos_tags[i].size() ? "end" : pos_tags[i][j];
+
+            pair<string, string> key = make_pair(first_word_pos, second_word_pos);
+            transition_to_count[key] += 1;
+            sum_of_first_word_pos_tag[first_word_pos] += 1;
+        }
+    }
+
+    for (auto key_value : transition_to_count) {
+        pair<string, string> key = key_value.first;
+        int value = key_value.second;
+
+        linear_trans_prob_matrix[key] = 1.0 * value / sum_of_first_word_pos_tag[key.first];
+
+        printf("[%8s, %8s] => %.3f \n", key.first.c_str(), key.second.c_str(), linear_trans_prob_matrix[key]);
+    }
+    
+    cout << "Size of translation matrix : " << transition_to_count.size() << endl;
+
+    return linear_trans_prob_matrix;
+}
+
+vector<string> viterbi(unordered_map<string, Word_emission> emission_matrix,
                        unordered_map<pair<string, string>, double, hash_pair> linear_trans_prob_matrix,
                        vector<string> sentence)
 {
@@ -37,7 +170,7 @@ vector<string> viterbi(unordered_map<string, pair<int, unordered_map<string, int
     for (const auto &word : sentence) 
     {
         unordered_map<string, double> temp;
-        for (const auto &word_tag_to_count : emission_matrix[word].second)
+        for (const auto &word_tag_to_count : emission_matrix[word].post_tag_to_count)
         {
             string state = word_tag_to_count.first;
             double max_prob = -INFINITY;
@@ -46,7 +179,7 @@ vector<string> viterbi(unordered_map<string, pair<int, unordered_map<string, int
             {
                 // Next type
                 double trans_prob = linear_trans_prob_matrix[{prev_state, state}];
-                double emission_prob = get_emission_probability(emission_matrix[word], state);
+                double emission_prob = emission_matrix[word].probability(state);
                 double score = log(trans_prob) + log(emission_prob);
 
                 max_prob = max(max_prob, score);
@@ -66,114 +199,4 @@ vector<string> viterbi(unordered_map<string, pair<int, unordered_map<string, int
 
     posSentence.push_back("end");
     return posSentence;
-}
-
-int main()
-{
-    ifstream data("../data.txt");
-    ifstream posdata("../pos.txt");
-    vector<vector<string>> sentences;
-    vector<vector<string>> pos;
-    string line;
-
-    if (!data.is_open() || !posdata.is_open()) {
-        cerr << "Error opening file" << endl;
-        return 1;
-    }
-
-    cout << "Processing Data .." << endl;
-    while (getline(data, line)) {
-        if (!line.empty()) {
-            istringstream iss(line);
-            vector<string> words((istream_iterator<string>(iss)), istream_iterator<string>());
-            sentences.push_back(words);
-        }
-    }
-    while (getline(posdata, line)) {
-        if (!line.empty()) {
-            vector<string> words_pos;
-            stringstream ss(line);
-            string word_pos;
-            while (getline(ss, word_pos, ',')) {
-                words_pos.push_back(word_pos);
-            }
-            pos.push_back(words_pos);
-        }
-    }
-    cout << "Number of sentences processed: " << sentences.size() << endl;
-    cout << "Number of pos processed: " << pos.size() << endl;
-
-    /*
-     *   Building of the emission matrix.
-     */
-    unordered_map<string, pair<int, unordered_map<string, int>>> emission_matrix;
-
-    #pragma omp parallel for schedule(dynamic, 10)
-    for (int i = 0; i < sentences.size(); i++) {
-
-        if (sentences[i].size() != pos[i].size()) {
-            cerr << "Words and POS mismatch on line : " << i << endl;
-            continue;
-        }
-
-        for (int j = 0; j < sentences[i].size(); j++)
-        {
-            string word = sentences[i][j];
-            string pos_tag = pos[i][j];
-
-            emission_matrix[word].second[pos_tag] += 1;
-            emission_matrix[word].first += 1;
-        }
-    }
-
-    cout << "Sentence map of : " << emission_matrix.size() << " unique words." << endl;
-    cout << "fish --> verb : " << get_emission_probability(emission_matrix["fish"], "verb") << ", noun :" << get_emission_probability(emission_matrix["fish"], "noun") << endl;
-
-    /*
-     *  Building of the transition matrix.
-     */
-    unordered_map<pair<string, string>, int, hash_pair> translation_matrix;
-    unordered_map<string, int> sum_of_first_word_pos;
-
-    for (int i = 0; i < pos.size(); i++) {
-        for (int j = 0; j < pos[i].size(); j++) {
-            string first_word_pos;
-            string second_word_pos;
-
-            first_word_pos = j == 0 ? "start" : pos[i][j - 1];
-            second_word_pos = j == pos[i].size() ? "end" : pos[i][j];
-
-            pair<string, string> key = make_pair(first_word_pos, second_word_pos);
-            translation_matrix[key] += 1;
-            sum_of_first_word_pos[first_word_pos] += 1;
-        }
-    }
-
-    unordered_map<pair<string, string>, double, hash_pair> linear_trans_prob_matrix;
-
-    for (auto key_value : translation_matrix) {
-        pair<string, string> key = key_value.first;
-        int value = key_value.second;
-
-        linear_trans_prob_matrix[key] = 1.0 * value / sum_of_first_word_pos[key.first];
-
-        printf("[%8s, %8s] => %.3f \n", key.first.c_str(), key.second.c_str(), linear_trans_prob_matrix[key]);
-    }
-    
-    cout << "Size of translation matrix : " << translation_matrix.size() << endl;
-    
-    
-    
-    /*
-     *  Execution of Viterbi
-     */
-    vector<string> test = {"tom", "loves", "fish"};
-    test = viterbi(emission_matrix, linear_trans_prob_matrix, test);
-    cout << "result : " << endl;
-    for (const auto &word : test)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-    return 0;
 }
